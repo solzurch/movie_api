6e0be24c323405674f821351f8105e66365ecaed
solzurch <151952240+solzurch@users.noreply.github.com>
2024-02-18 21:26:21 +0100

    update index.js

--- index.js
+++ index.js
@@ -67,6 +67,10 @@ app.put(
   "/users/:Username",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
+    // CONDITION TO CHECK ADDED HERE
+    if (req.user.Username !== req.params.Username) {
+      return res.status(400).send("Permission denied");
+    }
     await Users.findOneAndUpdate(
       { Username: req.params.Username },
       {
