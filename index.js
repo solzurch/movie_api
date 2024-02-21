97ea56b4ce44f14ff9641c60e553e55ba000dd1b
solzurch <151952240+solzurch@users.noreply.github.com>
2024-02-21 22:44:10 +0100

    debug

--- index.js
+++ index.js
@@ -296,7 +296,7 @@ app.use((err, req, res, next) => {
 
 // listen for requests
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
- console.log('Listening on Port ' + port);
+app.listen(port,  () => {
+  console.log("Listening on port " + port);
 });
 
